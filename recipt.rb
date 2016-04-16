65d56be84c40167eeb15228a928e2f93a1fe2cdd
Poro Shadows <sickan843@live.se>
2016-04-16 13:55:51 +0200

    Update recipt.rb
    Removed verbose do ends and added non tested cron job.

--- recipt.rb
+++ recipt.rb
@@ -1,45 +1,32 @@
 # Update apt
-execute 'sudo apt-get update' do
-end
+execute 'sudo apt-get update'
 
 # Instal packages
-apt_package 'omxplayer' do
-  action :install
-end
+apt_package 'omxplayer'
 
-apt_package 'python3.4' do
-  action :install
-end
+apt_package 'python3.4'
 
-apt_package 'python-imaging' do
-  action :install
-end
+apt_package 'python-imaging'
 
-apt_package 'python-imaging-tk' do
-  action :install
-end
+apt_package 'python-imaging-tk'
 
-apt_package 'unclutter' do
-  action :install
-end
+apt_package 'unclutter'
 
-apt_package 'mplayer' do
-  action :install
-end
+apt_package 'mplayer'
 
-apt_package 'uzbl' do
-  action :install
-end
+apt_package 'uzbl'
 
 # Execute scripts
-execute "sudo curl https://yt-dl.org/downloads/2016.04.13/youtube-dl -o /usr/local/bin/youtube-dl" do
-end
+execute "sudo curl https://yt-dl.org/downloads/2016.04.13/youtube-dl -o /usr/local/bin/youtube-dl"
 
-execute "sudo chmod a+rx /usr/local/bin/youtube-dl" do
-end
+execute "sudo chmod a+rx /usr/local/bin/youtube-dl"
 
-execute 'sh /home/pi/InfoTVChef/clone-git-repo.sh' do
-end
+execute 'sh /home/pi/InfoTVChef/clone-git-repo.sh'
 
-execute 'sudo sh /home/pi/InfoTVChef/add-lines.sh "python /home/pi/pipresents/pipresents.py -f -b -p fria_infotv_1p3" /home/pi/.config/lxsession/LXDE-pi/autostart' do
+execute 'sudo sh /home/pi/InfoTVChef/add-lines.sh "python /home/pi/pipresents/pipresents.py -f -b -p fria_infotv_1p3" /home/pi/.config/lxsession/LXDE-pi/autostart'
+
+# Experimantal. Not tested.
+cron 'network-login' do
+  minute '0'
+  command 'python /home/pi/FriaInfoTV/infotv-network.py Guest fria123'
 end
